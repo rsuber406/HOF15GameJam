6035f868767f511107246c701f2d771cfb46ebf6
Ryan <120147127+rsuber406@users.noreply.github.com>
2025-03-09 13:48:18 -0400

    Base GameManger made

---
+++ HOFGameJam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private GameObject lossScreen;
+
+    [SerializeField] private GameObject winScreen;
+
+    [SerializeField] private GameObject creditsScreen;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
