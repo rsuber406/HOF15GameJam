86716ce38b4eb9378cf90fabf402a7c6c5d4be13
Ryan <120147127+rsuber406@users.noreply.github.com>
2025-03-09 16:39:48 -0400

    Light can orbit around the area it is set at

--- HOFGameJam/Assets/Scripts/AI/LightFollow.cs
+++ HOFGameJam/Assets/Scripts/AI/LightFollow.cs
@@ -5,17 +5,22 @@ using Vector3 = UnityEngine.Vector3;
 public class LightFollow : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-   [SerializeField] private float speed = 1.5f;
+    [SerializeField] private float speed = 1.5f;
+    [SerializeField] private float timeCheckDelayForPosition = 0.05f;
     private Transform target;
-    private float timeCheckDelayForPosition = 0.05f;
     private float timeToUpdate = 0;
     private bool updatePosition = false;
-    private Transform referencePosition;
+    [SerializeField] private Transform referencePosition;
     private Vector3 targetDirection;
     private float distanceToTarget;
+    private bool isMoving = false;
+    private Transform currentOrbitingTransform;
+    private Transform originalTransform;
+    private Vector3 orbitAround;
+
     void Start()
     {
-        timeToUpdate = timeCheckDelayForPosition;
+        originalTransform = referencePosition;
     }
 
     void Update()
@@ -24,6 +29,19 @@ public class LightFollow : MonoBehaviour
         {
             LerpToTarget();
         }
+
+        if (timeToUpdate >= timeCheckDelayForPosition && !updatePosition && !isMoving)
+        {
+            CenterChangePoint();
+            timeToUpdate = 0;
+        }
+
+        if (isMoving)
+        {
+            UpdateStoppedLocation(orbitAround);
+        }
+
+        timeToUpdate += Time.deltaTime;
     }
 
     public void ChangePosition()
@@ -39,12 +57,38 @@ public class LightFollow : MonoBehaviour
         if (Vector3.Distance(transform.position, referencePosition.position) >= 1f)
         {
             float randomYOffet = Random.Range(-2, 2);
-            Vector3 directionTowardsTarget = new Vector3(referencePosition.position.x, referencePosition.position.y + randomYOffet, referencePosition.position.z);
-            transform.position = Vector3.MoveTowards(transform.position, directionTowardsTarget, Time.deltaTime * speed);
+            Vector3 directionTowardsTarget = new Vector3(referencePosition.position.x,
+                referencePosition.position.y + randomYOffet, referencePosition.position.z);
+            transform.position =
+                Vector3.MoveTowards(transform.position, directionTowardsTarget, Time.deltaTime * speed);
         }
-        else updatePosition = false;
+        else
+        {
+            updatePosition = false;
+        }
     }
 
+    void UpdateStoppedLocation(Vector3 targetPosition)
+    {
+        float randomYOffet = Random.Range(-2, 2);
+        if (Vector3.Distance(transform.position, targetPosition) >= 1f)
+        {
+            Vector3 directionTowardsTarget = new Vector3(targetPosition.x,
+                targetPosition.y + randomYOffet, targetPosition.z);
+            transform.position =
+                Vector3.MoveTowards(transform.position, directionTowardsTarget, Time.deltaTime * speed);
+        }
 
+        else isMoving = false;
+    }
 
+    void CenterChangePoint()
+    {
+        Transform reference = GameManager.GetInstance().GetLightCurrentPosition();
+        Vector3 targetLocation = Random.insideUnitCircle * 3f;
+        targetLocation += reference.position;
+        isMoving = true;
+        orbitAround = targetLocation;
+        UpdateStoppedLocation(targetLocation);
+    }
 }
