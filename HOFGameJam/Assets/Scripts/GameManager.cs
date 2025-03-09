1ba45e4e87b0a8b77f8af1063c34780a64ad291c
DakotaCottman <djcottman@gmail.com>
2025-03-09 18:21:37 -0400

    ui prefab and button added

--- HOFGameJam/Assets/Scripts/GameManager.cs
+++ HOFGameJam/Assets/Scripts/GameManager.cs
@@ -1,34 +1,81 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
     [SerializeField] private GameObject player;
-
-    [SerializeField] private GameObject lossScreen;
-
+    [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject winScreen;
-
     [SerializeField] private GameObject creditsScreen;
-    
-    [SerializeField]  private Transform [] lightPositions;
-
-    private static GameManager instance;
+    [SerializeField] private Transform[] lightPositions;
+    [SerializeField] private Button startButton;
+    public static GameManager instance;
 
     private int guidedTransform = 0;
+    private bool isPaused = true;
+    
     void Awake()
     {
         instance = this;
+        
+        ShowCursor();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        Time.timeScale = 0;
+        
+        ShowCursor();
+        
+        if (startButton != null)
+        {
+            startButton.onClick.AddListener(OnStartButtonClick);
+        }
+       
+    }
+    
     void Update()
     {
-        
+        if (isPaused)
+        {
+            ShowCursor();
+        }
+    }
+    private void ShowCursor()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    private void HideCursor()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void OnStartButtonClick()
+    {
+        StateResume();
     }
 
-   public static GameManager GetInstance()
+    public void StatePause()
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+        ShowCursor();
+    }
+
+    public void StateResume()
+    {
+        isPaused = false;
+        Time.timeScale = 1;
+        HideCursor();
+        
+        if (mainMenu != null)
+        {
+            mainMenu.SetActive(false);
+        }
+    }
+
+    public static GameManager GetInstance()
     {
         return instance;
     }
@@ -48,7 +95,7 @@ public class GameManager : MonoBehaviour
 
     public Transform GetLightCurrentPosition()
     {
-        Transform current =  lightPositions[guidedTransform];
+        Transform current = lightPositions[guidedTransform];
         return current;
     }
 }
