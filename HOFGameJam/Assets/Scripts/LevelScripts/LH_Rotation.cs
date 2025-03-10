db2d39c7c7202f4fb2b2644543140ce68a633afa
AV-Cables <ajmcdo618@gmail.com>
2025-03-09 23:10:03 -0400

    Lighthouse Code Begun

---
+++ HOFGameJam/Assets/Scripts/LevelScripts/LH_Rotation.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+
+public class LH_Rotation : MonoBehaviour
+{
+    [SerializeField] private GameObject Flaps;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Flaps.transform.rotation()
+    }
+}
